b0ff8bb251c376e7c2d1e7f226ff656c3fd7564b
YeonSeoJo <irwinjoebeck@gmail.com>
2022-08-14 23:58:02 +0900

    first ts commit2

--- first.ts
+++ first.ts
@@ -1,2 +1,11 @@
-let a: string = 'hello';
-a = 1234;
+const a: string = '5';
+const b: number = 5;
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+
+// function add(x:number, y:number): number {return x + y};
+// const add: (x:number, y: number) => number = (x, y) => x + y;
+// const obj: { lat: number, lon:number} = { lat: 37.5, lon: 127.5};
+
+// 타입스크립트는 자바스크립트의 변수, 매개변수, 리턴값에 타입을 지정하는 것
